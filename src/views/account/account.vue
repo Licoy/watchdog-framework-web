db9bfde6d299924ac469260e91daad5a816dfb47
Licoy <licoycn@gmail.com>
2018-04-26 09:45:23 +0800

    更新个人中心页面，TODO

--- src/views/account/account.vue
+++ src/views/account/account.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-      <h1>个人账户</h1>
+      <Card>
+        <p slot="title" class="card-title">
+            <Icon type="home"></Icon>
+            <span>个人中心</span>
+        </p>
+        <div>
+            个人用户页 开发中...
+        </div>
+    </Card>
   </div>
 </template>
 <script>
