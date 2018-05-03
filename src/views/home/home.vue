d87b645eedd3a50928175d1be2c8b7016414f389
Licoy <licoycn@gmail.com>
2018-05-03 15:07:59 +0800

    更新主页

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -6,9 +6,39 @@
             <span>首页</span>
         </p>
         <div>
-          <Icon type="social-github"></Icon>
-          <a target="_blank" href="https://github.com/Licoy/watchDog-framework">watchDog-framework</a>
-           - 基于vue+iview+springboot+shiro+mybatis+mybatis-plus+HikariCP开发的基础权限框架
+          <h2>介绍</h2>
+          <div class="margin-bottom-10">
+            watchDog-framework - 基于SpringBoot+Shiro+Mybatis+Mybatis-Plus+HikariCP+Redis+Vue+iView等开发的轻量级管理系统快速开发脚手架,
+            拥有角色、用户、资源管理、同时数据更新时关联的用户相应的权限也会实时更新，并且此项目会进行持续更新升级，欢迎使用。
+          </div>
+          <h2>项目地址</h2>
+          <div class="margin-bottom-10" style="margin-left:20px">
+            <ul>
+              <li>Github：<a target="_blank" href="https://github.com/Licoy/watchDog-framework">https://github.com/Licoy/watchDog-framework</a></li>
+              <li>码云：<a target="_blank" href="https://gitee.com/licoy/watchdog-framework">https://gitee.com/licoy/watchdog-framework</a></li>
+            </ul>
+          </div>
+          <h2>讨论</h2>
+          <div class="margin-bottom-10" style="margin-left:20px">
+              <ul>
+                <li>QQ群：30261540 <a target="_blank" href="https://shang.qq.com/wpa/qunwpa?idkey=c3541f1d0dbe443456228e3aebf23f6795b614a94d5df6a32f0b2b1c759bb99b">点我直接加入</a></li>
+                <li>作者博客：<a target="_blank" href="https://www.licoy.cn/">https://www.licoy.cn/</a></li>
+              </ul>
+          </div>
+          <h2>BUG反馈</h2>
+          <div class="margin-bottom-10" style="margin-left:20px">
+              <ul>
+                <li><a target="_blank" href="https://github.com/watchdog-framework/watchdog-framework/issues">Github上进行反馈</a></li>
+                <li><a target="_blank" href="https://gitee.com/licoy/watchdog-framework/issues">码云上进行反馈</a></li>
+                <li><a target="_blank" href="https://shang.qq.com/wpa/qunwpa?idkey=c3541f1d0dbe443456228e3aebf23f6795b614a94d5df6a32f0b2b1c759bb99b">QQ群进行反馈</a></li>
+              </ul>
+          </div>
+          <h2>赞助</h2>
+          <div class="margin-bottom-10" style="margin-left:20px">
+              <ul>
+                <li><a target="_blank" href="https://www.licoy.cn/sponsor.html">点此进行项目赞助</a></li>
+              </ul>
+          </div>
         </div>
       </Card>
   </div>
